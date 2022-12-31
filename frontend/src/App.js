c6b6b3ce45a5e72b00e594abc8baed6334cd14a2
Shubham Chaudhari <shubhamchaudhari@Shubhams-MacBook-Air.local>
2022-12-31 22:39:24 +0530

    added product detail screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
 import React from 'react'
-import Footer from './Components/Footer';
-import Header from './Components/Header'
-import { Container } from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header'
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+
 
 const App = () => {
   return (
-    <>
+    <Router>
+
       <Header></Header>
       
       <main className='py-3'>
         <Container>
-        <h1>Hello this is welcome page</h1>
+        <Routes>
+          <Route path='/' element={<HomeScreen/>}/>
+          <Route path='/product/:id' element={<ProductScreen/>} />
+        </Routes>
         </Container>
       
       </main>
       <Footer></Footer>
-    </>
+
+      
+    </Router>
   );
 };
 export default App;
--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">MyShop</Navbar.Brand>
+          <Link to="/">
+            <Navbar.Brand>MyShop</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-              <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
+              <Nav.Link as={NavLink} to="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/login">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>

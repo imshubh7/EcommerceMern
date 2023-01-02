1eef460740b7d1c4fab1754513d5f3fddf4baf1e
Shubham <ishubhchaudhari@gmail.com>
2023-01-02 22:45:07 +0530

    added mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 import express from "express";
-
-import products from "./data/products.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
+app.get("/", (req, res) => {
+  res.send("Server is Running...");
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen( PORT, console.log(`Server is running on PORT: ${PORT} in ${process.env.NODE_ENV} environment`));
+app.listen(
+  PORT,
+  console.log(
+    `Server is running on PORT: ${PORT} in ${process.env.NODE_ENV} environment`
+      .yellow.bold
+  )
+);
